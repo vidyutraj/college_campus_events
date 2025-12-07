a8561c5c556a7721576201e0fce112f3afef7228
Evelyn Chen <evelynchen555@gmail.com>
2025-12-06 19:04:37 -0500

    Npm run build URL fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
-    base: command === 'serve' ? '/' : '/static/',
-
     server: {
       proxy: {
         '/api': {
